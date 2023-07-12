81ef8567bb3e1534dae4c3ba801c9cd002ffc5f0
JeffBrockStudio <jeff@jeffbrockstudio.com>
2023-07-12 10:52:20 -0700

    Added Team CPT and ACF.

--- functions.php
+++ functions.php
@@ -43,3 +43,145 @@ remove_filter( 'the_content', 'wpautop' );
  * Theme shortcodes.
 */
 require get_stylesheet_directory() . '/inc/shortcodes/shortcodes.php';
+
+
+/**
+ * Register custom post types.
+ */
+function custom_create_post_types() {
+
+	// Team Member
+	register_post_type( 'team',
+		array(
+			'labels' => array(
+				'name'               => __( 'Team Member' ),
+				'singular_name'      => __( 'Team Member' ),
+				'menu_name'          => __( 'Team Members' ),
+				'name_admin_bar'     => __( 'Team Member' ),
+				'add_new'            => __( 'Add New' ),
+				'add_new_item'       => __( 'Add New Team Member' ),
+				'new_item'           => __( 'New Team Member' ),
+				'edit_item'          => __( 'Edit Team Member' ),
+				'view_item'          => __( 'View Team Member' ),
+				'all_items'          => __( 'All Team Members' ),
+				'search_items'       => __( 'Search Team Members' ),
+				'parent_item_colon'  => __( 'Parent Team Members:' ),
+				'not_found'          => __( 'No team members found.' ),
+				'not_found_in_trash' => __( 'No team members found in Trash.' )
+			),
+			'menu_icon' => 'dashicons-groups',			
+			'public' => true,
+			'hierarchical' => true,
+			'supports' => array( 'title', 'editor', 'thumbnail' ),
+			'has_archive' => true			
+		)
+	);	
+	
+	// Publications
+	register_post_type( 'resource',
+		array(
+			'labels' => array(
+				'name'               => __( 'Publications' ),
+				'singular_name'      => __( 'Publication' ),
+				'menu_name'          => __( 'Publications' ),
+				'name_admin_bar'     => __( 'Publication' ),
+				'add_new'            => __( 'Add New' ),
+				'add_new_item'       => __( 'Add New Publication' ),
+				'new_item'           => __( 'New Publication' ),
+				'edit_item'          => __( 'Edit Publication' ),
+				'view_item'          => __( 'View Publication' ),
+				'all_items'          => __( 'All Publications' ),
+				'search_items'       => __( 'Search Publications' ),
+				'parent_item_colon'  => __( 'Parent Publications:' ),
+				'not_found'          => __( 'No publications found.' ),
+				'not_found_in_trash' => __( 'No publications found in Trash.' )
+			),
+			'menu_icon' => 'dashicons-calendar-alt',			
+			'public' => true,
+			'hierarchical' => true,
+			'supports' => array( 'title', 'editor', 'thumbnail', 'page-attributes' ),
+			'has_archive' => true			
+		)
+	);	
+}
+add_action( 'init', 'custom_create_post_types' );
+
+
+/**
+ * Add excerpt to custom post types.
+ */
+function custom_add_excerpt_support_for_cpt() {
+ add_post_type_support( 'project', 'excerpt' );
+}
+//add_action( 'init', 'custom_add_excerpt_support_for_cpt' );
+
+
+/**
+ * Add custom taxonomies
+ */
+function custom_add_custom_taxonomies() {
+	
+	// Team Roles
+	register_taxonomy('team_roles', 'team', 
+		array(
+			'labels' => array(
+				'name'                       => __( 'Team Roles' ),
+				'singular_name'              => __( 'Team Role' ),
+				'menu_name'                  => __( 'Team Roles' ),
+				'all_items'                  => __( 'All Team Roles' ),
+				'parent_item'                => __( 'Parent Team Role' ),
+				'parent_item_colon'          => __( 'Parent Team Role' ),
+				'new_item_name'              => __( 'New Team Role Name' ),
+				'add_new_item'               => __( 'Add Team Role' ),
+				'edit_item'                  => __( 'Edit Team Role' ),
+				'update_item'                => __( 'Update Team Role' ),
+				'view_item'                  => __( 'View Team Role' ),
+				'separate_items_with_commas' => __( 'Separate team roles with commas' ),
+				'add_or_remove_items'        => __( 'Add or remove team roles' ),
+				'choose_from_most_used'      => __( 'Choose from the most used' ),
+				'popular_items'              => __( 'Popular Team Roles' ),
+				'search_items'               => __( 'Search Team Roles' ),
+				'not_found'                  => __( 'Not Found' ),
+				'no_terms'                   => __( 'No items' ),
+				'items_list'                 => __( 'Team Roles list' ),
+				'items_list_navigation'      => __( 'Team Roles list navigation' )
+			),		
+			'label' => __( 'Team Roles' ),
+			'hierarchical' => true		
+		)
+	);	
+	
+	// Seasons
+	register_taxonomy('seasons', 'event', 
+		array(
+			'labels' => array(
+				'name'                       => __( 'Seasons' ),
+				'singular_name'              => __( 'Season' ),
+				'menu_name'                  => __( 'Seasons' ),
+				'all_items'                  => __( 'All Seasons' ),
+				'parent_item'                => __( 'Parent Season' ),
+				'parent_item_colon'          => __( 'Parent Season' ),
+				'new_item_name'              => __( 'New Season Name' ),
+				'add_new_item'               => __( 'Add Season' ),
+				'edit_item'                  => __( 'Edit Season' ),
+				'update_item'                => __( 'Update Season' ),
+				'view_item'                  => __( 'View Season' ),
+				'separate_items_with_commas' => __( 'Separate seasons with commas' ),
+				'add_or_remove_items'        => __( 'Add or remove seasons' ),
+				'choose_from_most_used'      => __( 'Choose from the most used' ),
+				'popular_items'              => __( 'Popular Seasons' ),
+				'search_items'               => __( 'Search Seasons' ),
+				'not_found'                  => __( 'Not Found' ),
+				'no_terms'                   => __( 'No items' ),
+				'items_list'                 => __( 'Seasons list' ),
+				'items_list_navigation'      => __( 'Seasons list navigation' )
+			),		
+			'label' => __( 'Seasons' ),
+			'hierarchical' => true		
+		)
+	);
+
+};
+add_action( 'init', 'custom_add_custom_taxonomies', 0 );
+
+
