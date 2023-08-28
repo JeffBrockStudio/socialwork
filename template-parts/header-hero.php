8311cc9a427b470e6bde9f29624106b7995806c0
Jeff Brock <jeff@jeffbrockstudio.com>
2023-08-27 23:58:01 -0700

    Changed Academics to Admissions.

--- template-parts/header-hero.php
+++ template-parts/header-hero.php
@@ -3,7 +3,7 @@
     <div class="image-large" style="background-image: url(https://socialwork.local/wp-content/uploads/2023/07/recent-news.jpg)"></div>
     <div class="card-body">
       <div class="inner-card-body">
-        <h2 class="card-title">ACADEMICS</h2>
+        <h2 class="card-title">ADMISSIONS</h2>
       </div>
     </div>
   </div>
